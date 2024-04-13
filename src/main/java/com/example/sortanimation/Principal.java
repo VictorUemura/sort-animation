e00963678b28e598538b7d38141f9f7852bfc69c
VictorUemura <victoruemura04@hotmail.com>
2024-04-12 22:39:06 -0300

    refatoracao e criacao da funcao de mover botoes em qualquer posicao + organizacao do codigo

--- src/main/java/com/example/sortanimation/Principal.java
+++ src/main/java/com/example/sortanimation/Principal.java
@@ -18,7 +18,7 @@ public class Principal extends Application {
     Button botao_inicio;
     // vetor que e exibido
     private Button vet[];
-    private static int TAMVET = 12;
+    private static int TAMVET = 16;
     private int vetInt[];
     private int TL;
 
@@ -26,6 +26,20 @@ public class Principal extends Application {
         launch(args);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        configura_botao_inicio();
+        configura_pane();
+        geraVetor();
+        configura_exibicao_vetor();
+
+        stage.setTitle("Bucket Sort e Tim Sort");
+        Scene scene = new Scene(pane, 1000, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // configuracoes de botoes e funcoes para insercao de elementos na tela a seguir:
     public void configura_botao_inicio() {
         botao_inicio = new Button();
         botao_inicio.setLayoutX(10);
@@ -45,9 +59,32 @@ public class Principal extends Application {
         add_pane(botao_inicio);
     }
 
+    // configuracoes de exibicao e geracao de vetor em forma de botoes a seguir:
+    public boolean existe(int num) {
+        int i = 0;
+        while (i < TL && vetInt[i] != num)
+            i++;
+        return i != TL;
+    }
+
+    public int geraNumero(int min, int max) {
+        Random random = new Random();
+        int value;
+        do {
+            value = random.nextInt(min, max);
+        } while (existe(value));
+        return value;
+    }
+
+    public void geraVetor() {
+        TL = 0;
+        vetInt = new int[TAMVET];
+        while (TL < TAMVET)
+            vetInt[TL++] = geraNumero(1, 100);
+    }
+
     public void configura_exibicao_vetor() {
-        // configura exibicao do vetor
-        int x = 100, y = 200, tamAltura = 40, tamComprimento = 40, numFonte = 14, distancia = 42;
+        int x = 100, y = 200, tamAltura = 40, tamComprimento = 40, numFonte = 14, distancia = 45;
         vet = new Button[TAMVET];
         for (int i = 0; i < TAMVET; i++) {
             vet[i] = new Button(vetInt[i] + "");
@@ -62,75 +99,64 @@ public class Principal extends Application {
         }
     }
 
-    public void geraVetor() {
-        TL = 0;
-        vetInt = new int[TAMVET];
-        while (TL < TAMVET)
-            vetInt[TL++] = geraNumero(1, 100);
-    }
+    // configuracoes e funcoes da animacao a seguir:
+    public void troca_botoes(int pos1, int pos2) {
+        double posInicial1 = vet[pos1].getLayoutX(), posInicial2 = vet[pos2].getLayoutX();
+        int distPerc = Math.abs((int) (posInicial2 - posInicial1) / 5);
+        System.out.println(distPerc);
 
-    public int geraNumero(int min, int max) {
-        Random random = new Random();
-        int value;
-        do {
-            value = random.nextInt(min, max);
-        } while (existe(value));
-        return value;
-    }
+        for (int i = 0; i < 10; i++) {
+            Platform.runLater(() -> vet[pos1].setLayoutY(vet[pos1].getLayoutY() + 5));
+            Platform.runLater(() -> vet[pos2].setLayoutY(vet[pos2].getLayoutY() - 5));
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pos1 < pos2) {
+            for (int i = 0; i < distPerc; i++) {
+                Platform.runLater(() -> vet[pos1].setLayoutX(vet[pos1].getLayoutX() + 5));
+                Platform.runLater(() -> vet[pos2].setLayoutX(vet[pos2].getLayoutX() - 5));
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            for (int i = 0; i < distPerc; i++) {
+                Platform.runLater(() -> vet[pos1].setLayoutX(vet[pos1].getLayoutX() - 5));
+                Platform.runLater(() -> vet[pos2].setLayoutX(vet[pos2].getLayoutX() + 5));
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            Platform.runLater(() -> vet[pos1].setLayoutY(vet[pos1].getLayoutY() - 5));
+            Platform.runLater(() -> vet[pos2].setLayoutY(vet[pos2].getLayoutY() + 5));
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-    public boolean existe(int num) {
-        int i = 0;
-        while (i < TL && vetInt[i] != num)
-            i++;
-        return i != TL;
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Pesquisa e Ordenacao");
-        configura_botao_inicio();
-        configura_pane();
-        geraVetor();
-        configura_exibicao_vetor();
-        Scene scene = new Scene(pane, 800, 600);
-        stage.setScene(scene);
-        stage.show();
+        Button aux = vet[pos1];
+        vet[pos1] = vet[pos2];
+        vet[pos2] = aux;
     }
 
     public void move_botoes() {
         Task<Void> task = new Task<Void>() {
             @Override
             protected Void call() {
-                for (int i = 0; i < 10; i++) {
-                    Platform.runLater(() -> vet[0].setLayoutY(vet[0].getLayoutY() + 5));
-                    Platform.runLater(() -> vet[1].setLayoutY(vet[1].getLayoutY() - 5));
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (int i = 0; i < 16; i++) {
-                    Platform.runLater(() -> vet[0].setLayoutX(vet[0].getLayoutX() + 5));
-                    Platform.runLater(() -> vet[1].setLayoutX(vet[1].getLayoutX() - 5));
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (int i = 0; i < 10; i++) {
-                    Platform.runLater(() -> vet[0].setLayoutY(vet[0].getLayoutY() - 5));
-                    Platform.runLater(() -> vet[1].setLayoutY(vet[1].getLayoutY() + 5));
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                Button aux = vet[0];
-                vet[0] = vet[1];
-                vet[1] = aux;
+                int pos1 = 0, pos2 = 10;
+                troca_botoes(pos1, pos2);
+
                 return null;
             }
         };
